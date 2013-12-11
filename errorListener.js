34360ab5c713ad42137959554221e7c827576314
locksley <scottli_010@hotmail.com>
2013-12-10 23:29:54 -0800

    removed error handler, tricked manifest

--- errorListener.js
+++
@@ -1,4 +0,0 @@
-window.onerror = function( message, url, lineNumber ) {
-	console.log( message, url, lineNumber )
-	return window.location = "/error.html?" + "message=" + message + "&url=" + url + "&lineNumber=" + lineNumber
-};
